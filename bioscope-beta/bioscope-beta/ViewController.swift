a6c8988b5d350464c84815f5a717f150a9a3ce69
raymondsjkim <raymond.sj.kim@gmail.com>
2018-09-18 18:31:00 -0700

    added project

---
+++ bioscope-beta/bioscope-beta/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  bioscope-beta
+//
+//  Created by Raymond Kim on 9/18/18.
+//  Copyright © 2018 Raymond Kim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
